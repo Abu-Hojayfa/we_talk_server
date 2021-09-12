d4f59b5bbb13f2ee83dd5457aca64a18473bd318
Abu-Hojayfa <abu.hojayfa100@gmail.com>
2021-09-12 13:09:38 +0600

    simple

--- index.js
+++ index.js
@@ -2,21 +2,37 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server, { cors: { origin: '*' } });
+const io = new Server(server, { cors: { origin: "*" } });
 
 require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('message' , msg);
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://Boss:hemal001@cluster0.pec8g.mongodb.net/we-talk?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client.db("we-talk").collection("testing");
+
+  io.on("connection", (socket) => {
+    socket.on("chat message", (msg) => {
+      collection.insertOne({ msg: msg });
+    });
+
+    socket.on("get message", () => {
+      collection.find({}).toArray((err, mssgs) => {
+        io.emit("message", mssgs);
+      });
+    });
   });
 });
-
 
 server.listen(port, () => console.log(`Running on ${port}`));
